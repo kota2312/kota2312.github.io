94fe7948645e3458f500651522f92ac109499df0
kota2312 <bass_love_guitar@yahoo.co.jp>
2021-04-24 10:26:27 +0900

    追加

--- BookMarklet/selectSeek.js
+++ BookMarklet/selectSeek.js
@@ -1,11 +1,13 @@
 export default () => {  
     var faSelect = document.getElementsByTagName('select');
-    for (var i = 0; i < faSelect.length; i ++) {
-        if(faSelect[i]) {
-            alert("プルダウンあり。");
-            break;
-        } else {
-            alert("プルダウンなし");
+    if(faSelect[0]) {
+        for (var i = 0; i < faSelect.length; i ++) {
+            if(faSelect[i]) {
+                alert("プルダウンあり。");
+                break;
+            }
         }
-    }   
+    } else {
+        alert("プルダウンなし");
+    } 
 }
