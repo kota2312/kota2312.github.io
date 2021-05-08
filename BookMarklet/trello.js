9cbec3991c67166f1c4418496e0df7a9c1a8269f
kota2312 <55619911+kota2312@users.noreply.github.com>
2021-05-08 10:56:02 +0900

    Update trello.js

--- BookMarklet/trello.js
+++ BookMarklet/trello.js
@@ -31,4 +31,4 @@ for(var j = 0; j < kotaCard.length; j ++) {
 
 /* 計算式 全体から自分の行った案件の割合を出す。*/
 var fakeisan = tkCount / count * 100;
-console.log(Math.floor(fakeisan) + "%");
+(Math.floor(fakeisan) + "%");
